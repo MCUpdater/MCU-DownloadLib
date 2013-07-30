a3c0471010def0b08ef59005189b6dc5fcc6ed26
Scott M. Barbour <smbarbour@gmail.com>
2013-07-30 16:45:11 -0500

    Downloadable class should be functional

--- src/org/mcupdater/Downloadable.java
+++ src/org/mcupdater/Downloadable.java
@@ -1,46 +1,101 @@
 package org.mcupdater;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
 import java.util.List;
 
 public class Downloadable {
-	private String friendlyName;
-	private String filename;
-	private String md5;
-	private List<URL> downloadURLs;
+	private final String friendlyName;
+	private final String filename;
+	private final String md5;
+	private final List<URL> downloadURLs;
+	private final ProgressTracker tracker;
 	
 	public Downloadable(String friendlyName, String filename, String md5, List<URL> downloadURLs) {
-		setFriendlyName(friendlyName);
-		setFilename(filename);
-		setMD5(md5);
-		this.downloadURLs = downloadURLs; 
+		this.friendlyName = friendlyName;
+		this.filename = filename;
+		this.md5 = md5;
+		this.downloadURLs = downloadURLs;
+		this.tracker = new ProgressTracker();
 	}
 
 	public String getFriendlyName() {
 		return friendlyName;
 	}
 
-	public void setFriendlyName(String friendlyName) {
-		this.friendlyName = friendlyName;
-	}
-
 	public String getFilename() {
 		return filename;
-	}
-
-	public void setFilename(String filename) {
-		this.filename = filename;
 	}
 
 	public String getMD5() {
 		return md5;
 	}
 
-	public void setMD5(String md5) {
-		this.md5 = md5;
-	}
-	
 	public List<URL> getURLs() {
 		return this.downloadURLs;
+	}
+	
+	public void download(File basePath) throws IOException {
+		String localMD5 = null;
+		File resolvedFile = null;
+		
+		if (basePath != null && (!basePath.isDirectory())) {
+			basePath.mkdirs();			
+		}
+		resolvedFile = new File(basePath, this.filename);
+		if (resolvedFile.isFile()) {
+			localMD5 = getMD5(resolvedFile);
+		}
+		
+		if (resolvedFile.isFile() && !resolvedFile.canWrite()) {
+			throw new RuntimeException("No write permissions for " + resolvedFile.toString() + "!");
+		}
+		if (localMD5 == this.md5) {
+			//TODO Log entry: No download necessary
+			return;
+		}
+		Iterator<URL> iteratorURL = downloadURLs.iterator();
+		while (iteratorURL.hasNext()){
+		try {
+				URL localURL = iteratorURL.next();
+				URLConnection conn = localURL.openConnection();
+				conn.connect();
+				if (conn.getContentLength() > 0) {
+					this.tracker.setTotal(conn.getContentLength());
+				}
+				InputStream input = new TrackingInputStream(conn.getInputStream(), this.tracker);
+				FileOutputStream output = new FileOutputStream(resolvedFile);
+				IOUtils.copy(input, output);
+				IOUtils.closeQuietly(input);
+				IOUtils.closeQuietly(output);
+				localMD5 = getMD5(resolvedFile);
+				if (localMD5 == this.md5) {
+					//TODO Log entry: Download successful
+					return;
+				}				
+			} catch (IOException e) {
+				//TODO Log warning: Error during connection
+			}
+		}
+		throw new RuntimeException("Unable to download (" + this.friendlyName + ") - All known URLs failed.");
+	}
+	
+	public static String getMD5(File file) throws IOException {
+		byte[] hash;
+		InputStream is = new FileInputStream(file);
+		hash = DigestUtils.md5(is);
+		is.close();
+		
+		return new String(Hex.encodeHex(hash));		
 	}
 }
